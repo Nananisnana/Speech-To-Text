12f8d798882f4a3d34e703caed0f799c551d6a88
nananisnana <nisapaydin004@gmail.com>
2025-10-31 17:10:15 +0300

    Proje dosyası ekle.

---
+++ speechto/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace speechto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbMetin = new System.Windows.Forms.RichTextBox();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbMetin
+            // 
+            this.rtbMetin.BackColor = System.Drawing.SystemColors.Info;
+            this.rtbMetin.Location = new System.Drawing.Point(12, 12);
+            this.rtbMetin.Name = "rtbMetin";
+            this.rtbMetin.Size = new System.Drawing.Size(776, 426);
+            this.rtbMetin.TabIndex = 0;
+            this.rtbMetin.Text = "";
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(490, 269);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(244, 113);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "Dinlemeyi Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Location = new System.Drawing.Point(51, 274);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(251, 102);
+            this.btnDurdur.TabIndex = 2;
+            this.btnDurdur.Text = "Dinlemeyi Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(341, 98);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(122, 16);
+            this.lblDurum.TabIndex = 3;
+            this.lblDurum.Text = "Durum: Beklemede";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.rtbMetin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMetin;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
+
---
+++ speechto/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using System.Windows.Forms;
+using System.Configuration;
+
+
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace speechto
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognizer recognizer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnDurdur.Enabled = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDurum.Text = "Durum: Azure'a bağlanmaya hazır.";
+        }
+        private async void btnBaslat_Click(object sender, EventArgs e)
+        {
+            string subscriptionKey = ConfigurationManager.AppSettings["AzureSpeechKey"];
+            string region = ConfigurationManager.AppSettings["AzureSpeechRegion"];
+            var config = SpeechConfig.FromSubscription(subscriptionKey, region);
+            config.SpeechRecognitionLanguage = "tr-TR";
+            using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+            {
+                recognizer = new SpeechRecognizer(config, audioConfig);
+            }
+            recognizer.Recognized += Recognizer_Recognized; 
+            recognizer.SessionStarted += Recognizer_SessionStarted; 
+            recognizer.SessionStopped += Recognizer_SessionStopped; 
+            await recognizer.StartContinuousRecognitionAsync();
+
+            btnBaslat.Enabled = false;
+            btnDurdur.Enabled = true;
+        }
+        private async void btnDurdur_Click(object sender, EventArgs e)
+        {
+            await recognizer.StopContinuousRecognitionAsync();
+            recognizer.Recognized -= Recognizer_Recognized;
+            recognizer.SessionStarted -= Recognizer_SessionStarted;
+            recognizer.SessionStopped -= Recognizer_SessionStopped;
+            recognizer.Dispose();
+            recognizer = null;
+
+            btnBaslat.Enabled = true;
+            btnDurdur.Enabled = false;
+            lblDurum.Text = "Durum: Durduruldu.";
+        }
+        private void Recognizer_SessionStarted(object sender, SessionEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                lblDurum.Text = "Durum: Azure dinliyor...";
+            }));
+        }
+        private void Recognizer_SessionStopped(object sender, SessionEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                lblDurum.Text = "Durum: Oturum kapandı.";
+            }));
+        }
+        private void Recognizer_Recognized(object sender, SpeechRecognitionEventArgs e)
+        {
+            if (e.Result.Reason == ResultReason.RecognizedSpeech)
+            {
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    rtbMetin.AppendText(e.Result.Text + " ");
+                }));
+            }
+            else if (e.Result.Reason == ResultReason.NoMatch)
+            {
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    lblDurum.Text = "Durum: Konuşma anlaşılamadı.";
+                }));
+            }
+        }
+    }
+}
